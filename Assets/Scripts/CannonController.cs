24a0b406740b5069e3a6e2f9ef1700968db48261
nne-nne <adam.leczkowski1@gmail.com>
2021-08-23 23:38:28 +0200

    Cannon controller

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -23,7 +23,6 @@ public class CannonController : MonoBehaviour
             Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
             if(bulletRb != null)
             {
-                Debug.Log("adding force");
                 bulletRb.velocity = bullet.transform.forward * bulletForce;
             }
 
@@ -31,8 +30,17 @@ public class CannonController : MonoBehaviour
         }
     }
 
+    private void TraceMouse()
+    {
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+    }
+
     void Update()
     {
+        TraceMouse();
+
         if(Input.GetMouseButtonDown(0))
         {
             Shoot();
