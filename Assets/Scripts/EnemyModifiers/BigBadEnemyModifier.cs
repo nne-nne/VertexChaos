6d142b692cc61f85f8257d7013d3458126ecec48
PUtkala <51992192+PUtkala@users.noreply.github.com>
2021-08-29 01:57:43 +0200

    balans

--- Assets/Scripts/EnemyModifiers/BigBadEnemyModifier.cs
+++ Assets/Scripts/EnemyModifiers/BigBadEnemyModifier.cs
@@ -8,7 +8,7 @@ public class BigBadEnemyModifier : EnemyModifier
     public override void create_effect(GameObject enemy)
     {
         EnemyController enemy_sc = enemy.GetComponent<EnemyController>();
-        enemy_sc.maxHealth += 40 + 10*strenght;
+        enemy_sc.maxHealth += 10 + 5*strenght;
         enemy_sc.health = enemy_sc.maxHealth;
         enemy.transform.localScale += new Vector3(1.5f+strenght/4, 1.5f + strenght / 4, 1.5f + strenght / 4);
         enemy_sc.timeBetweenShots *= 1 / (1+strenght/100);
