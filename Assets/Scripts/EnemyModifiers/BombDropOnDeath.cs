14c1abdd5fffbca34c1a4363b965551f8bcb70fb
PUtkala <51992192+PUtkala@users.noreply.github.com>
2021-08-28 19:07:10 +0200

    dodatkowe modyfikatory

---
+++ Assets/Scripts/EnemyModifiers/BombDropOnDeath.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemies;
+
+public class BombDropOnDeath : EnemyModifier
+{
+    public override void destroy_effect(GameObject enemy)
+    {
+
+        GameObject new_bullet = ObjectPool.SharedInstance.GetPooledObject();
+        if (new_bullet != null)
+        {
+            GameObject Player = GameObject.FindGameObjectsWithTag("Player")[0];
+            new_bullet.transform.position = enemy.transform.position;
+            new_bullet.transform.LookAt(Player.transform);
+
+            BulletSc new_bulletSc = new_bullet.GetComponent<BulletSc>();
+
+            if (new_bulletSc != null)
+            {
+                List<BulletModifier> new_bms = new List<BulletModifier>();
+                new_bms.Add(new ExplosionModifier(new GameObject()));
+                new_bulletSc.damage = 1;
+                new_bulletSc.speed *= 0.5f;
+                new_bulletSc.lifetime = 2f;
+
+                new_bulletSc.affiliation = Affiliation.Enemy;
+                new_bulletSc.Activate();
+                new_bulletSc.AddModifiers(new_bms);
+                new_bulletSc.Shoot();
+                new_bullet.transform.localScale *= 1 + strenght/3;
+
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/EnemyModifiers/LowerTimeBetweenShotsModifier.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemies;
+
+public class LowerTimeBetweenShotsModifier : EnemyModifier
+{
+    public override void create_effect(GameObject enemy)
+    {
+        EnemyController enemy_sc = enemy.GetComponent<EnemyController>();
+        enemy_sc.timeBetweenShots *= 1 / (1 + strenght / 25);
+    }
+}
