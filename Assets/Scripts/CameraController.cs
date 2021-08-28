98cd51d9ed0e6c0de8285a0a630df1d5312f0f76
nne-nne <adam.leczkowski1@gmail.com>
2021-08-29 01:13:02 +0200

    CameraController
    parameters should be like:
    mouse follow radius 0.8
    camera swing multiplier 0.01

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
-    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float mouseFollowRadiusFrac;
+    [SerializeField] private float cameraSwingMultiplier;
+
+    private float mouseFollowRadius;
 
     private Vector3 targetPos;
 
     void Awake()
     {
         targetPos = transform.position;
+        mouseFollowRadius = Screen.height * mouseFollowRadiusFrac;
+    }
+
+    private Vector3 FindCameraTarget()
+    {
+        Vector2 offset = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2);
+        if (Mathf.Abs(offset.x) > mouseFollowRadius) offset.x = mouseFollowRadius * Mathf.Sign(offset.x);
+        if (Mathf.Abs(offset.y) > mouseFollowRadius) offset.y = mouseFollowRadius * Mathf.Sign(offset.y);
+        offset *= cameraSwingMultiplier;
+        Debug.Log(offset);
+        return new Vector3(offset.x, 0f, offset.y);
     }
 
     void Update()
@@ -23,7 +37,9 @@ public class CameraController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //transform.position = targetPos;
-        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+        //targetPos = Vector3.Lerp(transform.position,
+        //    FindCameraTarget(), cameraSpeed);
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z) + FindCameraTarget();
+        //transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
     }
 }
